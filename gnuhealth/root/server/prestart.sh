9e585817f3591e8812cdc31ad8fab5264bf6072e
Ledo Kun <30885977+LedoKun@users.noreply.github.com>
2020-06-14 21:07:46 +0700

    Fix typo

--- gnuhealth/root/server/prestart.sh
+++ gnuhealth/root/server/prestart.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Correcting files ownership..."
 chown -R gnuhealth: /server/attach
-chown -R gnuhealth: /server/gnuhealth/log
+chown -R gnuhealth: /server/gnuhealth/logs
 
 echo "Starting Tryton server..."
 su - gnuhealth -c "/bin/bash /server/start.sh"
