fe2dd0bacf2b6d84fa6fc1dbfa5a4588b0f5ca6f
LedoKun <romamp100@gmail.com>
2020-06-15 16:30:17 +0000

    Updated gnuhealth container

--- gnuhealth/root/server/prestart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Correcting files ownership..."
-chown -R gnuhealth: /server/attach
-chown -R gnuhealth: /server/gnuhealth/logs
-
-echo "Starting Tryton server..."
-su - gnuhealth -c "/bin/bash /server/start.sh"
